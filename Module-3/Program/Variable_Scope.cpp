706a91b8304e401af944491cfea82d8e48e076d0
bhavya0093 <bkaanjana9@gmail.com>
2025-08-23 08:43:43 +0530

    Update Variable_Scope.cpp

--- Module-3/Program/Variable_Scope.cpp
+++ Module-3/Program/Variable_Scope.cpp
@@ -1,22 +1,17 @@
-#include <iostream>
+#include <iostream>#include <iostream>
 using namespace std;
 
 // Global variable
 int num = 10;
 
-void show() {
-    cout << "Inside show, Global num = " << num << endl;
-}
-
 int main() {
     // Local variable
-    int num = 20;
+    int num = 20;  
 
-    cout << "Inside main, Local num = " << num << endl;
-    cout << "Inside main, Global num = " << ::num << endl;
-
-    show();
+    cout << "Local num = " << num << endl;       
+    cout << "Global num = " << ::num << endl;    
 
     return 0;
 }
+
 
